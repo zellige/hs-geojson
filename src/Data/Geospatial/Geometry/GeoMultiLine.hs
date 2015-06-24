fd5a18d43253caff5777967c72704548633bdfc0
Artem M. Chirkin <chirkin@arch.ethz.ch>
2015-06-24 18:49:13 +0200

    Major changes in parsing (Line->LineString,MultiLine->MultiLineString,fixed nested coordinates in Multi*)

--- src/Data/Geospatial/Geometry/GeoMultiLine.hs
+++ src/Data/Geospatial/Geometry/GeoMultiLine.hs
@@ -12,27 +12,39 @@ module Data.Geospatial.Geometry.GeoMultiLine (
         GeoMultiLine(..)
     -- * Lenses
     ,   unGeoMultiLine
+    -- * To Points
+    ,   splitGeoMultiLine, mergeGeoLines
     ) where
 
 import Data.Geospatial.BasicTypes
 import Data.Geospatial.Geometry.GeoLine
 import Data.Geospatial.Geometry.Aeson
+import Data.LineString
 
 import Control.Lens ( makeLenses )
 import Control.Monad ( mzero )
 import Data.Aeson ( FromJSON(..), ToJSON(..), Value(..), Object )
 
-newtype GeoMultiLine    = GeoMultiLine { _unGeoMultiLine :: [GeoLine] } deriving (Show, Eq)
+newtype GeoMultiLine    = GeoMultiLine { _unGeoMultiLine :: [LineString GeoPositionWithoutCRS] } deriving (Show, Eq)
 
 makeLenses ''GeoMultiLine
+
+
+-- | Split GeoMultiLine coordinates into multiple GeoLines
+splitGeoMultiLine:: GeoMultiLine -> [GeoLine]
+splitGeoMultiLine = map GeoLine . _unGeoMultiLine
+
+-- | Merge multiple GeoLines into one GeoMultiLine
+mergeGeoLines :: [GeoLine] -> GeoMultiLine
+mergeGeoLines = GeoMultiLine . map _unGeoLine
 
 -- instances
 
 instance ToJSON GeoMultiLine where
 --  toJSON :: a -> Value
-    toJSON = makeGeometryGeoAeson "MultiLine" . _unGeoMultiLine
+    toJSON = makeGeometryGeoAeson "MultiLineString" . _unGeoMultiLine
 
 instance FromJSON GeoMultiLine where
 --  parseJSON :: Value -> Parser a
-    parseJSON (Object o)    = readGeometryGeoAeson "MultiLine" GeoMultiLine o
+    parseJSON (Object o)    = readGeometryGeoAeson "MultiLineString" GeoMultiLine o
     parseJSON _             = mzero
